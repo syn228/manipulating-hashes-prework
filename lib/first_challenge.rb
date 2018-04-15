f84be071654fcc88694ba313cdfa436d8f933f92
Aaron Na <syn228@nyu.edu>
2018-04-15 20:57:04 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
 contacts.each do |name, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-    value.delete_if("strawberry")
+    value.delete_if(value == "strawberry")
     end
   end
 end
