c144ca8be79cc23c168ddd2c5d54b7955897385d
Aaron Na <syn228@nyu.edu>
2018-04-15 21:18:25 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,13 @@ def first_challenge
   }
 
   #your code here
-
+contacts.each do |name, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+    value.delete("strawberry")
+    end
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,8 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
+unmerged = groceries.values
+merged = unmerged.flatten
+  merged
 
 end
