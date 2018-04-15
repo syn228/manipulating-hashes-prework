8cacafe8f7f3ef47feed977c7d09765473242e7d
Aaron Na <syn228@nyu.edu>
2018-04-15 21:02:33 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
 contacts.each do |name, data|
   data.each do |attribute, value|
-    if attribute == :favorite_icecream_flavors && value.include?("strawberry")
+    if attribute == :favorite_icecream_flavors
     value.delete("strawberry")
     end
   end
